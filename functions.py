090cb713ebf1562ea026d2d1e74b47925b479c8c
bunny <bunnyaluvala@gmail.com>
2024-04-25 18:34:17 -0400

    Weather Forecast App

--- functions.py
+++ functions.py
@@ -9,10 +9,6 @@ def get_data(place, forecast_days=None, kind=None):
     filtered_content = content["list"]
     nr_values = 8 * forecast_days
     filtered_content = filtered_content[:nr_values]
-    if kind == "Temperature":
-        filtered_content = [dict["main"]["temp"]for dict in filtered_content]
-    if kind == "Sky":
-        filtered_content = [dict["weather"][0]["main"]for dict in filtered_content]
     return filtered_content
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -7,12 +7,26 @@ place = st.text_input("place...")
 days = st.slider("Forcast Days",min_value=1,max_value=5,
                  help="number of days to be forecasted")
 options = st.selectbox("Select date to view",
-                      ("temperature","sky"))
+                      ("Temperature","Sky"))
 st.subheader(f"{options} for the next {days} days in {place}")
 
-data = get_data(place,days,options)
+if place:
+    try:
+        filtered_content = get_data(place, days)
 
-d,t =get_data(days)
+        if options == "Temperature":
+            temperatures = [dict["main"]["temp"]/ 10 for dict in filtered_content]
+            dates = [dict["dt_txt"]for dict in filtered_content]
+            figure = px.line(x=dates,y=temperatures,labels={"x":"dates","y":"temperatures (c)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d,y=t,labels={"x":"dates","y":"temperatures (c)"})
-st.plotly_chart(figure)
+        if options == "Sky":
+            images = {"Clear": "images (2)/clear.png","Clouds":"images (2)/cloud.png",
+                      "Rain":"images (2)/rain.png","Snow":""}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_content]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths,width=115)
+
+    except KeyError:
+        st.write("Wrong input")
